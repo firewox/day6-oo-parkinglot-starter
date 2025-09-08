3a25ed8d71d1675785100da610fa45cdd40513db
LIJA30 <jade.li@oocl.com>
2025-09-08 21:55:24 +0800

    refactor: story-1-2-3-4 code

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class ParkingLotTest {
-    //Case 1 - Given a parking lot, and a car, When park the car, Then return a parking ticket.
+    //story1- Case 1 - Given a parking lot, and a car, When park the car, Then return a parking ticket.
     @Test
     void should_return_a_parking_ticket_when_parking_the_car_given_parking_log_a_car() {
         //Given
@@ -20,7 +20,7 @@ class ParkingLotTest {
         assertEquals(ticket, parkingTicket);
     }
 
-    //Case 2 - Given a parking lot with a parked car, and a parking ticket, When fetch the car, Then return the parked car.
+    //story1- Case 2 - Given a parking lot with a parked car, and a parking ticket, When fetch the car, Then return the parked car.
     @Test
     void should_return_the_parked_car_when_fetch_the_car_given_a_parking_lot_with_a_picked_car() {
         //Given
@@ -34,7 +34,7 @@ class ParkingLotTest {
     }
 
 
-    //Case 3 - Given a parking lot with two parked cars, and two parking tickets, When fetch the car twice, Then return the right car with each ticket
+    //story1- Case 3 - Given a parking lot with two parked cars, and two parking tickets, When fetch the car twice, Then return the right car with each ticket
 
     @Test
     void should_return_the_right_car_with_each_ticket_when_fetch_the_car_twice_given_a_parking_lot_with_two_parked_cars_and_two_parking_tickets() {
@@ -56,7 +56,7 @@ class ParkingLotTest {
         assertEquals(null, parkingLot.fetchCar(ticket2));
     }
 
-    //Case 4 - Given a parking lot, and a wrong parking ticket, When fetch the car, Then return nothing.
+    //story1- Case 4 - Given a parking lot, and a wrong parking ticket, When fetch the car, Then return nothing.
     @Test
     void should_return_nothing_when_fetch_car_given_a_parking_lot_and_a_wrong_parking_ticket() {
         //Given
@@ -69,7 +69,7 @@ class ParkingLotTest {
         assertEquals(null, pickUpCar);
     }
 
-    //Case 5 - Given a parking lot, and a used parking ticket, When fetch the car, Then return nothing.
+    //story1- Case 5 - Given a parking lot, and a used parking ticket, When fetch the car, Then return nothing.
     @Test
     void should_return_nothing_when_fetch_car_given_a_parking_lot_and_a_used_parking_ticket() {
         //Given
@@ -83,7 +83,7 @@ class ParkingLotTest {
         assertEquals(null, parkingLot.fetchCar(ticket));
     }
 
-    //Case 6 - Given a parking lot without any position, and a car, When park the car, Then return nothing
+    //story1- Case 6 - Given a parking lot without any position, and a car, When park the car, Then return nothing
     @Test
     void should_return_nothing_when_park_car_given_a_parking_lot_without_any_position_and_a_car() {
         //Given
@@ -96,7 +96,7 @@ class ParkingLotTest {
         assertEquals(null, parkingTicket);
     }
 
-    //case1- Given a unrecognized ticket, When fetch the car, Then return unrecognized parking ticket
+    //story2- case1- Given a unrecognized ticket, When fetch the car, Then return unrecognized parking ticket
     @Test
     void should_return_unrecognized_ticket_message_when_fetch_car_given_a_unrecognized_parking_ticket() {
         //Given
@@ -111,7 +111,7 @@ class ParkingLotTest {
         assertNull(pickupCar);
     }
 
-    //case2- Given a unused ticket, When fetch the car, Then return unrecognized parking ticket
+    //story2- case2- Given a unused ticket, When fetch the car, Then return unrecognized parking ticket
     @Test
     void should_return_unrecognized_ticket_message_when_fetch_car_given_a_unused_parking_ticket() {
         //Given
@@ -127,7 +127,7 @@ class ParkingLotTest {
         assertEquals("unrecognized parking ticket", message);
     }
 
-    //case3- Given a parking log, and a car, When park car and parking log without position, Then return no available position message
+    //story3:case1- Given a parking log, and a car, When park car and parking log without position, Then return no available position message
     @Test
     void should_return_no_available_position_ticket_message_when_park_car_and_parking_log_without_position_given_a_parking_log_and_a_car() {
         //Given
@@ -142,7 +142,7 @@ class ParkingLotTest {
         assertEquals("no available position message", message);
     }
 
-    //story3：case1- Given a parking log, and a car, and a parking boy, When park car, Then return a parking ticket
+    //story3：case2- Given a parking log, and a car, and a parking boy, When park car, Then return a parking ticket
     @Test
     void should_return_a_parking_ticket_when_park_car_given_a_parking_log_and_a_car_and_a_parking_boy() {
         //Given
@@ -155,7 +155,7 @@ class ParkingLotTest {
         assertEquals(parkingTicket,ticket);
     }
 
-    //story3：case2- Given a parking log, and a ticket, and a parking boy, When fetch car, Then return a car
+    //story3：case3- Given a parking log, and a ticket, and a parking boy, When fetch car, Then return a car
     @Test
     void should_return_a_car_when_fetch_car_given_a_parking_log_and_a_ticket_and_a_parking_boy() {
         //Given
@@ -168,7 +168,7 @@ class ParkingLotTest {
         assertEquals(car, pickupCar);
     }
 
-    //story3：case2- Given a parking log with two parked cars and two parking tickets, When fetch car twice, Then return right car with each ticket
+    //story3：case4- Given a parking log with two parked cars and two parking tickets, When fetch car twice, Then return right car with each ticket
     @Test
     void should_return_the_right_car_with_each_ticket_when_fetch_the_car_twice_given_a_parking_lot_with_two_parked_cars_and_two_parking_tickets_and_parking_boy() {
         //Given
@@ -189,7 +189,7 @@ class ParkingLotTest {
         assertEquals(null, parkingBoy.fetchCar(ticket2));
     }
 
-    // Given: two parking lots, and a parking boy; When first parking lot is full; Then park car to second parking lot;
+    // story4: case1- Given: two parking lots, and a parking boy; When first parking lot is full; Then park car to second parking lot;
     @Test
     void should_park_car_to_second_parking_lot_when_first_parking_lot_is_full_given_two_parking_lots_and_a_parking_boy(){
         //Given

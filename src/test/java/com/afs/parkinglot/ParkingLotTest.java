afc1703f672a38c0725306afcaa6f4146743981d
LIJA30 <jade.li@oocl.com>
2025-09-08 20:52:23 +0800

    add: story-3 add parking boy fetch code junit test code

--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -153,5 +153,37 @@ class ParkingLotTest {
         assertEquals(parkingTicket,ticket);
     }
 
+    //story3：case2- Given a parking log, and a ticket, and a parking boy, When fetch car, Then return a car
+    @Test
+    void should_return_a_car_when_fetch_car_given_a_parking_log_and_a_ticket_and_a_parking_boy() {
+        //Given
+        Car car = new Car("123");
+        ParkingBoy parkingBoy = new ParkingBoy();
+        ParkingTicket ticket = parkingBoy.parkCar(car);
+        //When
+        Car pickupCar = parkingBoy.fetchCar(ticket);
+        //Then
+        assertEquals(car, pickupCar);
+    }
 
+    //story3：case2- Given a parking log with two parked cars and two parking tickets, When fetch car twice, Then return right car with each ticket
+    @Test
+    void should_return_the_right_car_with_each_ticket_when_fetch_the_car_twice_given_a_parking_lot_with_two_parked_cars_and_two_parking_tickets_and_parking_boy() {
+        //Given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car1 = new Car("123");
+        Car car2 = new Car("456");
+        ParkingTicket ticket1 = new ParkingTicket(car1, 1, parkingBoy.getParkingLot());
+        ParkingTicket ticket2 = new ParkingTicket(car2, 2, parkingBoy.getParkingLot());
+        parkingBoy.parkCar(car1);
+        parkingBoy.parkCar(car2);
+        //When
+        Car pickUpCar1  = parkingBoy.fetchCar(ticket1);
+        Car pickUpCar2  = parkingBoy.fetchCar(ticket2);
+        //Then
+        assertEquals(car1, pickUpCar1);
+        assertEquals(car2, pickUpCar2);
+        assertEquals(null, parkingBoy.fetchCar(ticket1));
+        assertEquals(null, parkingBoy.fetchCar(ticket2));
+    }
 }

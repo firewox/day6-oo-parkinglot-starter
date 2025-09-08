89f9f39d9ecfe436fcfee4ebd6b7e1cd780aaf8a
LIJA30 <jade.li@oocl.com>
2025-09-08 20:51:18 +0800

    add: story-3 imply parking boy code

--- src/main/java/com/afs/parkinglot/ParkingBoy.java
+++ src/main/java/com/afs/parkinglot/ParkingBoy.java
@@ -8,5 +8,50 @@ public class ParkingBoy {
     private Car car;
     private String notifyMessage;
 
+    public ParkingBoy() {
+        this.parkingLot = this.parkingLot == null ? PARKINGLOT : this.parkingLot;
+    }
 
+    public ParkingBoy(Car car, ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+        this.car = car;
+    }
+
+    public ParkingBoy(Car car) {
+        this.parkingLot = this.parkingLot == null ? PARKINGLOT : this.parkingLot;
+        this.car = car;
+    }
+
+
+    public ParkingTicket parkCar() {
+        if (Objects.isNull(this.car)) {
+            this.notifyMessage = "no car";
+            return null;
+        }
+        return this.parkingLot.parkCar(this.car);
+    }
+
+    public ParkingTicket parkCar(Car car) {
+        if (Objects.isNull(car)) {
+            this.notifyMessage = "no car";
+            return null;
+        }
+        return this.parkingLot.parkCar(car);
+    }
+
+    public Car fetchCar(ParkingTicket parkingTicket) {
+        return this.parkingLot.fetchCar(parkingTicket);
+    }
+
+    public String getNotifyMessage() {
+        return this.parkingLot.getNotifyMessage();
+    }
+
+    public ParkingLot getParkingLot() {
+        return parkingLot;
+    }
+
+    public void setParkingLot(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
 }
